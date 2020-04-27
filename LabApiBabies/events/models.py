eef95093ec5a7d4c381e8235f5bd2d0c072e4b3a
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-26 20:37:49 -0600

    Models correccion error

--- LabApiBabies/events/models.py
+++ LabApiBabies/events/models.py
@@ -7,7 +7,7 @@ class Baby(models.Model):
     # id creado por defecto
     eventType = models.CharField(max_length=20, null=False, blank=False)
     date = models.DateTimeField(null=False, blank=False, default=datetime.now)
-    note = models.charField(max_length=200, null=False, blank=False)
+    note = models.CharField(max_length=200, null=False, blank=False)
     baby = models.ForeignKey('babies.Baby', on_delete=models.SET_NULL, null=False, blank=False)
 
     def __str__(self):
