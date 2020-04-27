bdd7584a1f33ae212da85732a0bbbdaf06cd68af
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-26 22:31:31 -0600

    Viewsets

--- LabApiBabies/babies/views.py
+++ LabApiBabies/babies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from babies.models import Baby
+from babies.serializers import BabySerializer
+
 
 # Create your views here.
+class BabyViewSet(viewsets.ModelViewSet):
+    queryset = Baby.objects.all()
+    serializer_class = BabySerializer
+    
--- LabApiBabies/parents/views.py
+++ LabApiBabies/parents/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from parents.models import Parent
+from parents.serializers import ParentSerializer
 
 # Create your views here.
+class ParentViewSet(viewsets.ModelViewSet):
+    queryset = Parent.objects.all()
+    serializer_class = ParentSerializer
