0ee4916d97d0a96ca532212ea3cfe8b26a131da0
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-26 21:53:12 -0600

    Serializers

---
+++ LabApiBabies/events/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from events.models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = (
+            'id',
+            'eventType',
+            'date',
+            'note',
+            'baby'
+        )
