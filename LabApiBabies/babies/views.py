a6b385d783a93de16dfdedb2fab5392fd0a156e1
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-04-26 23:01:01 -0600

    Custom actions

--- LabApiBabies/babies/views.py
+++ LabApiBabies/babies/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from babies.models import Baby
+from events.models import Event
+
 from babies.serializers import BabySerializer
+from events.serializers import EventSerializer
 
 
 # Create your views here.
@@ -9,3 +15,10 @@ class BabyViewSet(viewsets.ModelViewSet):
     queryset = Baby.objects.all()
     serializer_class = BabySerializer
     
+@action(detail=True, methods=['get'])
+    def events(self, request, pk=None):
+        baby = self.get_object()
+        allEvents=[]
+        for event in Event.objects.filter(baby=baby):
+            allEvents.append(EventSerializer(event).data)
+        return Response(allEvents)
--- LabApiBabies/parents/views.py
+++ LabApiBabies/parents/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from parents.models import Parent
+from babies.models import Baby
+
 from parents.serializers import ParentSerializer
+from babies.serializers import BabySerializer
 
 # Create your views here.
 class ParentViewSet(viewsets.ModelViewSet):
     queryset = Parent.objects.all()
     serializer_class = ParentSerializer
+
+    # /parents/id/babies/
+    @action(detail=True, methods=['get'])
+    def babies(self, request, pk = None):
+        parent = self.get_object()
+        allBabies = []
+        for baby in Baby.objects.filter(parent=parent):
+            allBabies.append(BabySerializer(baby).data)
+        return Response(allBabies)
